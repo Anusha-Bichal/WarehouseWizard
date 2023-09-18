9b4c0d8f22a014c81299722a795a120d3e152c29
AnnabathinaMounika <112028867+AnnabathinaMounika@users.noreply.github.com>
2023-09-17 20:29:11 -0500

    added progress of our project to the repo

---
+++ Warehouse/Warehouse/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Warehouse.Models;
+
+namespace Warehouse.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly ILogger<AuthController> _logger;
+
+    public AuthController(ILogger<AuthController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return RedirectToAction("Login");
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+    
+    public IActionResult ResetPassword()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
