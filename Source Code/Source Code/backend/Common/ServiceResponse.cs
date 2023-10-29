f2b506b6b0d2a997848618453d287bb144a74288
Amani Chennu <112126839+amanichennu201@users.noreply.github.com>
2023-10-29 13:55:53 -0500

    Update ServiceResponse.cs

--- Source Code/Source Code/backend/Common/ServiceResponse.cs
+++ Source Code/Source Code/backend/Common/ServiceResponse.cs
@@ -2,7 +2,8 @@ namespace Backend.Common;
 
 public class ServiceResponse<T>
 {
-    public T? Data { get; set; }
+    
+public T? Data { get; set; }
     public bool Success { get; set; } = true;
     public string Message { get; set; } = string.Empty;
 
