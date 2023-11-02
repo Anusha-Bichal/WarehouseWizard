db2471cffe61727859938ee984afb4a930426960
Bear258 <111480360+Bear258@users.noreply.github.com>
2023-11-02 13:06:37 -0500

    Update Footer.tsx

--- Source Code/Source Code/frontend/app/components/Footer.tsx
+++ Source Code/Source Code/frontend/app/components/Footer.tsx
@@ -1,11 +1,13 @@
-import appConfig from "app.config"
+import appConfig from "./app.config";
 
 export function Footer() {
-	return (
-		<footer className="flex h-[44px] items-center justify-center px-6 py-1 text-center text-sm">
-			<span className="text-gray-400">
-				©{new Date().getFullYear()} {appConfig.name}, Inc. All rights reserved.
-			</span>
-		</footer>
-	)
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="flex h-[44px] items-center justify-center px-6 py-1 text-center text-sm">
+      <span className="text-gray-400">
+        {`©${currentYear} ${appConfig.name}, Inc. All rights reserved.`}
+      </span>
+    </footer>
+  );
 }
