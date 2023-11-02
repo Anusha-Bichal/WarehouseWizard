b46ab388e44cf808c588e2d4073780a57dc78335
Bear258 <111480360+Bear258@users.noreply.github.com>
2023-11-02 13:09:58 -0500

    Update EmptyState.tsx

--- Source Code/Source Code/frontend/app/components/ui/EmptyState.tsx
+++ Source Code/Source Code/frontend/app/components/ui/EmptyState.tsx
@@ -29,10 +29,11 @@ type EmptyStateProps = {
 
 export function EmptyState(props: EmptyStateProps) {
   const { label, icon } = props;
+
   return (
     <div className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center">
       <div className="flex items-center justify-center">
-        {icon ?? <XIcon size={70} className="text-gray-600" />}
+        {icon || <XIcon size={70} className="text-gray-600" />}
       </div>
       <span className="mt-4 block text-sm font-semibold text-gray-500">
         {label}
